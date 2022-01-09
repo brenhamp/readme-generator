71e4bbbddccc8553ef349de1bd8eb167a27891d4
brenhamp <bpozzi@trinity.edu>
2022-01-09 10:31:43 -0600

    added questions for users to fill out

--- .history/Develop/index_20220109101205.js
+++ .history/Develop/index_20220109101205.js
@@ -39,29 +39,11 @@ const questions = [
     },
 
     {
-        type: 'input',
-        name: 'credits',
-        message: 'List your collaborators, if any, with links to their GitHub profiles if desired. (optional)'
-    },
-
-    {
         type: 'list',
         name: 'license',
         message: 'Please select a license for this project.',
         //consult https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/licensing-a-repository
-        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0','postgresq1', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
-    },
-
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Please describe how users can contribute to your project. (optional)'
-    },
-
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'If you have tests for users to run, please enter them here and explain how to use them. (optional)'
+        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', ]
     }
 ];
 
